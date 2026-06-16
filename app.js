f139fc47cbb8664cfc2a3bbf23eb57bd8a122ad2
temuujinenkhbayar <temuujinenkhbayar125@gmail.com>
2026-06-16 03:34:00 +0000

    finished version

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
-//include Express
+// include Express
 const express = require('express');
 
-//server will listen on this port
+// server will listen on this port
 const port = 3000;
 
-//create instance of Express app
+// create instance of Express app
 const app = express();
 
-//ejs is templating engine
-app.set('view engine','ejs');
+// ejs is templating engine
+app.set('view engine', 'ejs');
 
-//this will allow us to serve up static files, CSS, images & JS
+// this will allow us to serve up static files, CSS, images & JS
 app.use(express.static(__dirname + '/public'));
 
-//index/home URL
-app.get('/',(req,res)=>{
-    let title = "Home Page";
-    res.render('pages/index',{'title': title});
+// home URL
+app.get('/', (req, res) => {
+    let title = "Solar System Home";
+    res.render('pages/index', { title: title });
 });
 
-//about URL
-app.get('/about',(req,res)=>{
-    let title = "About Page";
-    res.render('pages/about',{'title': title});
+// about URL
+app.get('/about', (req, res) => {
+    let title = "About This Website";
+    res.render('pages/about', { title: title });
 });
 
-//Set server to listen for requests
+// inner planets URL
+app.get('/inner-planets', (req, res) => {
+    let title = "Inner Planets";
+    res.render('pages/inner-planets', { title: title });
+});
+
+// outer planets URL
+app.get('/outer-planets', (req, res) => {
+    let title = "Outer Planets";
+    res.render('pages/outer-planets', { title: title });
+});
+
+// set server to listen for requests
 app.listen(port, () => {
-  console.log(`Server running at port: ${port}`);
+    console.log(`Server running at port: ${port}`);
 });
-
